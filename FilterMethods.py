b04016444bd3d62d09c27204f409b6e483cda437
Diego Melgoza <drmelgoza@icloud.com>
2026-06-01 17:56:54 -0700

    add method skeletons

---
+++ FilterMethods.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+"""
+methods to use (replace filler names when finalized):
+1. 
+2. 
+3. 
+4.
+"""
+
+def method_1():
+    ...
+
+def method_2():
+    ...
+
+def method_3():
+    ...
+
+def method_4():
+    ...
+
